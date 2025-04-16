d11513bc0ccf0633efa0344d8de1921633bcfa60
coordova <grcordova@gmail.com>
2025-04-15 20:28:13 -0400

    pruebas

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -108,6 +108,15 @@
             </div>
 
             {{-- Paginación --}}
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                </ul>
+            </nav>
             <div class="mt-4">
                 {{ $posts->links() }}
             </div>
